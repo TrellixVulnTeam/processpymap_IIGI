e6ce041a6ffe2871a0bf10c71cc26a423050a3ad
Azurevauban <azurewight@gmail.com>
2022-07-31 18:11:30 -0500

    implement & test out numeric input function

--- processpymap.py
+++ processpymap.py
@@ -3,14 +3,30 @@ class node:
     createdfrom = []
     """stores data within each part of the structure"""
     ingredient = ''
+    amountonhand = 0.00
+    amountmadepercraft = 0.00
+    amountneededpercraft = 0.00
 
     def __init__(myself, ingredient_name='', next=None):
+        """default constructor"""
         myself.usedtocreate = next
         myself.ingredient = ingredient_name
-
+        myself.amountonhand = 0.00
+        myself.amountmadepercraft = 0.00
+        myself.amountneededpercraft = 0.00
     def __del__(myself):
+        """default deconstructor"""
         print('deleted', myself.ingredient, 'from the tree')
-
+    def baz_tentative(myself,parent_ingredient = ''):
+        """setter for numeric variables"""
+        #todo test this out 
+        if not len(parent_ingredient) == 0:
+            myself.amountneededpercraft = float(input('What is the amount of',myself.ingredient,'needed to create',parent_ingredient,': '))
+            myself.amountneededpercraft = round(myself.amountneededpercraft, [0])
+        myself.amountonhand = float(input('What is the amount of',myself.ingredient,'that you have on hand: '))
+        myself.amountonhand = round(myself.amountonhand, [0])
+        myself.amountmadepercraft = float(input('What is the amount of',myself.ingredient,'made per craft: '))
+        myself.amountmadepercraft = round(myself.amountmadepercraft, [0])
 
 def foo_tentative(foo_Node=node):
     """add subingredient nodes to A"""
