fadb7744ff4aa5d753f3e07da34845412ed659f9
Azurevauban <azurewight@gmail.com>
2022-08-06 14:10:59 -0500

    commit

--- unittest.py
+++ unittest.py
@@ -1,6 +1,3 @@
-import traceback
-
-
 class Node:
     ingredient = ''
     amountonhand = 0
@@ -37,6 +34,7 @@ class Node:
             self.parentNode.traceback()
         else:
             print('NONE')
+            
     
 def searchforendpoints(a=Node):
     """search for endpoint nodes"""
@@ -88,19 +86,35 @@ def recursivearithmetic(currentNode = Node):
     if (currentNode.parentNode != None):
         recursivearithmetic(currentNode.parentNode)
     currentNode.amountresulted = D
-
+def printresultedamount(subjectNode = Node):
+    searchforendpoints(subjectNode)
+    print(subjectNode.ingredient,'amount resulted:',subjectNode.amountresulted)
 """beginning program"""
 print('\x1B[32mBeginning Process\x1B[37m\n')
 """create nodes"""
-        #! (A:amount on hand ,B:amount mader per craft,C:amount needed per craft)
-Silicon_Board = Node('Silicon Board', None, 310, 1, 4)  
+#! (A:amount on hand ,B:amount mader per craft,C:amount needed per craft)
+Focusing_Array = Node('Focusing Array',None,1,1,0)
+Advanced_Alloy = Node('Advanced Alloy',Focusing_Array,8,1,2)
+Crystal = Node('Crystal',Focusing_Array,640,1,2)
+Plasmic_Crystal = Node('Plasmic Crystal',Focusing_Array,41,2,2)
+Quantum_Processor = Node('Quantum Processor',Focusing_Array,20,2,1)
+Zerchesium_Bar = Node('Zerchesium Bar',Advanced_Alloy,548,1,1)
+Zerchesium_Ore = Node('Zerchesium Ore',Zerchesium_Bar,2,1,2)
+Protocite_Bar = Node('Protocite Bar',Advanced_Alloy,277,1,1)
+Protocite_Ore = Node('Protocite Ore',Protocite_Bar,2,1,2)
+Penumbrite_Shard = Node('Penumbrite Shard',Advanced_Alloy,86,1,1)
+Penumbrite_Ore = Node('Penumbrite Ore',Penumbrite_Shard,2,1,2)
+Lead = Node('Lead',Advanced_Alloy,251,1,1)
+Crystal_Plant_Seed = Node('Crystal Plant Seed',Crystal,100,1,1)
+Blood_Rock = Node('Blood Rock',Plasmic_Crystal,1000,1,50)
+Blood = Node('Blood',Blood_Rock,23,1,1)
+Lava = Node('Lava',Blood_Rock,404,1,1)
+Silicon_Board = Node('Silicon Board', Quantum_Processor, 310, 1, 4)  
 Copper_Wire = Node('Copper Wire', Silicon_Board, 492, 9, 1)
 Silicon = Node('Silicon', Silicon_Board, 1000, 1, 1)
 Copper_Bar = Node('Copper Bar', Copper_Wire, 1000, 1, 1)
 Copper_Ore = Node('Copper Ore', Copper_Bar, 2, 1, 2)
 Sand = Node('Sand', Silicon, 901, 1, 50)
-searchforendpoints(Silicon_Board)
-print(Silicon_Board.ingredient,'amount resulted:', Silicon_Board.amountresulted)
-print(Silicon.ingredient,'amount resulted:', Silicon.amountresulted)
+printresultedamount(Focusing_Array)
 """terminating process"""
 print('\x1B[31mI love you Narieles 💞\nI love you so freaking much you make me happy everyday!\x1B[37m')
